d3b9c29c026cec1a916e4b3013d9f701a5c3b17e
João Pedro <joao2015.76@gmail.com>
2022-06-17 20:15:41 -0300

    Fixed path error

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = commands.Bot('canacchi ')
 def load_cogs(bot):
     bot.load_extension('listeners.greetings')
 
-    for file in os.listdir("Bot/commands"):
+    for file in os.listdir("commands/"):
         if file.endswith(".py"):
             cog = file[:-3]
             bot.load_extension(f"commands.{cog}")
--- listeners/greetings.py
+++ listeners/greetings.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from datetime import datetime, timedelta
+from datetime import datetime
 
 class Listeners(commands.Cog):
     def __init__(self, bot):
