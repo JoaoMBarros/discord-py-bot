29e9551f4360cc3a160f74f56c1b08295f840064
João Pedro <joao2015.76@gmail.com>
2022-06-21 22:06:28 -0300

    Added poll function

--- commands/reactions.py
+++ commands/reactions.py
@@ -1,67 +1,77 @@
 from discord.ext import commands
 import asyncio
-import random
-
 class Reactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
     #This function needs a full rewrite. Way too polluted
-    @commands.command(name='votacao')
+    @commands.command(name='votacao', description='Faz uma votacao kk')
     async def start_poll(self, ctx):
-        string_list = str(ctx.message.content).split(' ')
-        string_list.pop(0)
-        string_list.pop(0)
+
+        #Breaking the string command and creating a list with all substrings
+        string_list = str(ctx.message.content)[17:]
+        string_list = string_list.split('^')
+
+        #Getting the timer
         time = int(string_list.pop(0))
-        poll_quantity = len(string_list)
+
+        poll_options = []
+        choosen_emotes = []
+        poll_string = ''
+
+        #Grabbing the emotes and the poll options sent with the command
+        while string_list:
+            choosen_emotes.append(string_list.pop(0))
+            poll_options.append(string_list.pop(0))
+        
+        #Striping the space characters from the elements of the emotes list
+        choosen_emotes = [s.strip(' ') for s in choosen_emotes]
+
+        #Creating the poll string        
+        for i in range(0, len(poll_options), 1):
+            poll_string += str(choosen_emotes[i]) + ' ➜ ' + str(poll_options[i]) + '\n'
+
+        poll_string = await ctx.send(poll_string)
+
         winners = []
         poll_emotes_count = []
-        choosen_emotes = []
-        msg = ''
         
-        #teste = str(string_list).split('-')
-        #await ctx.send('teste = ' + str(teste))
-        #await ctx.send('string_list: ' + str(string_list))
-
-        await ctx.send('Escolham entre:\n')
-        #Grabbing the emotes sent with the command
-        for item in string_list:
-            emote = str(item).split('-')
-            choosen_emotes.append(emote[0])
+        #Adding the reactions to it
+        for emote in choosen_emotes:
+            await poll_string.add_reaction(emote)
         
-        #Building the message that will be displayed for the poll
-        for i in range(0, poll_quantity, 1):
-            msg += string_list[i] + '\n'
-        
-        #Sending the message and adding the reactions to it
-        msg = await ctx.send(msg)
-        for i in range(0, poll_quantity, 1):
-            await msg.add_reaction(choosen_emotes[i])
-        
-        #time = await self.bot.wait_for('message')
-        #await ctx.send('Tempo ' + str(time))
+        #Time until the poll ends
         await asyncio.sleep(time)
-        msg = await msg.channel.fetch_message(msg.id)
-
-        #Getting the quantity of each reactiion
-        for reaction in msg.reactions:
+    
+        #Getting the quantity of each reaction
+        poll_string = await poll_string.channel.fetch_message(poll_string.id)
+        for reaction in poll_string.reactions:
             poll_emotes_count.append(reaction.count)
 
         #Getting the winner(s)
         aux = -1
-        for i in poll_emotes_count:
+        for i in poll_string.reactions:
             aux += 1
-            if i == max(poll_emotes_count):
-                winners.append(string_list[aux])
+            if i.count == max(poll_emotes_count):
+                winner_emote = choosen_emotes[aux]
+                winners.append(poll_options[aux])
+
+        winners = [s.strip(' ') for s in winners]
+        winner_message = 'Empate entre **'
 
         if len(winners) == 1:
-            winner_message = 'Vencedor: ' + winners[0]
+            winner_message = str(winner_emote) + ' ' + winners[0] + ' venceu.'
+        elif len(winners) == 2:
+            winner_message += str(winners[0]) + '** e **' + str(winners[1]) + '**.'
         else:
-            for i in winners:
-                winner_message = 'Empate!\nOpções empatadas: ' + ', '.join(winners)
-
+            for i in range(0, len(winners)):
+                if i < len(winners)-1:
+                    winner_message += winners[i] + '**, **'
+                else:
+                    winner_message = winner_message[:-6]
+                    winner_message += '** e **' + winners[-1] + '**.'
         await ctx.send(winner_message)
-
+        
     @commands.command(name="pingcana")
     async def send_ping_cana(self, ctx):
         msg = await ctx.channel.send('Eu devo pingar o cana?')
