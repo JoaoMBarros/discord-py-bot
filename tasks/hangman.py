2a76b7934e801264d6884272a7ded8d8991f5863
João Pedro <joao2015.76@gmail.com>
2022-06-26 01:22:25 -0300

    Finishing hangmangame

--- tasks/hangman.py
+++ tasks/hangman.py
@@ -1,6 +1,7 @@
 #Gotta rewrite this code...
 #Gotta redo the logic, game changed
 import asyncio
+from unidecode import unidecode
 from discord.ext import commands
 
 class Hangman(commands.Cog):
@@ -10,14 +11,21 @@ class Hangman(commands.Cog):
     @commands.max_concurrency(1,per=commands.BucketType.default,wait=False)
     @commands.command(name='hangman')
     async def hangman_game(self, ctx):
+        channel = self.bot.get_channel(716000401618370660)
         await ctx.send('Alimente o bot')
 
         def check_channel(m):
             return m.channel == ctx.message.channel
+        
+        def check_start(m):
+            if m.content == 'hangman parar':
+                return True
+            return m.content == 'hangman começar'
 
         food_string = await self.bot.wait_for('message', check=check_channel)
-        food_list = str(food_string.content).split('^')
+        await ctx.send('Bot alimentado')
 
+        food_list = str(food_string.content).split('^')
         aux_strip_items = []
         for element in food_list:
             aux_strip_items.append(element.strip())
@@ -28,23 +36,40 @@ class Hangman(commands.Cog):
     
         word_hint_dict = dict(new_food_list)
         words_to_be_guessed = list(word_hint_dict.keys())
-
-        while words_to_be_guessed:
+        hints = list(word_hint_dict.values())
+        users = []
         
-            characters_to_be_guessed = list(words_to_be_guessed[0])
-            guessing_gaps = []
-            users = []
+        start = await self.bot.wait_for('message', check=check_start)
 
-            msg = await ctx.send('Começando o jogo. React pra participar')
-            await msg.add_reaction('🔥')
-            await asyncio.sleep(5)
-            msg = await ctx.channel.fetch_message(msg.id)
+        if start.content == 'hangman parar':
+            await ctx.send('Jogo parado')
+            return
+        
+        msg = await channel.send('Começando o jogo. React pra participar')
+        await msg.add_reaction('🔥')
+        await asyncio.sleep(5)
+        msg = await channel.fetch_message(msg.id)
 
-            for reaction in msg.reactions:
-                async for user in reaction.users():
-                    if user != self.bot.user:
-                        users.append(user)
+        for reaction in msg.reactions:
+            async for user in reaction.users():
+                if user != self.bot.user:
+                    users.append(user)
+        
+        rank = {}
+
+        for key in users:
+            rank[key.name] = 0
+        
+        while words_to_be_guessed:
+
+            for player, score in rank.items():
+                string_rank = '**RANKING DA PARTIDA:**\n\n**{}**: {}'.format(player, score)
             
+            await ctx.send(string_rank)
+
+            guessing_gaps = []
+            characters_to_be_guessed = list(words_to_be_guessed[0])
+
             for i in range(0, len(words_to_be_guessed[0]), 1):
                 if characters_to_be_guessed[i] == ' ':
                     guessing_gaps.append(' ')
@@ -53,65 +78,77 @@ class Hangman(commands.Cog):
                 if i < len(words_to_be_guessed[0])-1:
                     guessing_gaps.append(' ')
 
-            await ctx.send('Palavra: `' + ''.join(guessing_gaps) + '`')
-            
-            
+            await asyncio.sleep(5)
+            await channel.send('A dica é: ' + hints[0] + '\nPalavra: `' + ''.join(guessing_gaps) + '`')
 
             def check(m):
-                if str(m.content).casefold() == 'hangman stop':
-                    exit(1)
-                return str(m.content).casefold() == words_to_be_guessed[0].casefold()
+                if m.content == 'hangman parar':
+                    return True
+                return str(unidecode(m.content)).casefold() == str(unidecode(words_to_be_guessed[0])).casefold()
             
             async def check_50_seconds():
                 channel = self.bot.get_channel(716000401618370660)
                 try:
-                    await self.bot.wait_for('message', check=check, timeout=10)
-                    return True
+                    msg = await self.bot.wait_for('message', check=check, timeout=10)
+                    return msg
                 except asyncio.TimeoutError:
-                    await channel.send('Primeira dica! A palavra começa com ' + characters_to_be_guessed[0])
                     guessing_gaps[0] = characters_to_be_guessed[0]
-                    await ctx.send('Palavra: `' + ''.join(guessing_gaps) + '`')
-                    await channel.send('Faltam 50 segundos!\n')
+                    await channel.send('A dica é: ' + hints[0] + '\nPalavra: `' + ''.join(guessing_gaps) + '`')
                     return False
 
             async def check_30_seconds():
                 channel = self.bot.get_channel(716000401618370660)
                 try:
-                    await self.bot.wait_for('message', check=check, timeout=20)
-                    return True
+                    msg = await self.bot.wait_for('message', check=check, timeout=20)
+                    return msg
                 except asyncio.TimeoutError:
-                    await channel.send('Segunda dica! A palavra termina com ' + characters_to_be_guessed[-1])
                     guessing_gaps[-1] = characters_to_be_guessed[-1]
-                    await ctx.send('Palavra: `' + ''.join(guessing_gaps) + '`')
-                    await channel.send('Faltam 30 segundos')
+                    await channel.send('A dica é: ' + hints[0] + '\nPalavra: `' + ''.join(guessing_gaps) + '`')
                     return False
             
             async def check_10_seconds():
                 channel = self.bot.get_channel(716000401618370660)
                 try:
-                    await self.bot.wait_for('message', check=check, timeout=10)
-                    return True
+                    msg = await self.bot.wait_for('message', check=check, timeout=10)
+                    return msg
                 except asyncio.TimeoutError:
                     await channel.send('Faltam 10 segundos')
                     return False
 
             try:
-                return_value_50 = await check_50_seconds()
+                msg = await check_50_seconds()
                 
-                if not return_value_50:
-                    return_value_30 = await check_30_seconds()
+                if not msg:
+                    msg = await check_30_seconds()
                 
-                    if not return_value_30:
-                        return_value_10 = await check_10_seconds()
+                    if not msg:
+                        msg = await check_10_seconds()
 
-                        if not return_value_10:
+                        if not msg:
                             msg = await self.bot.wait_for('message', check=check, timeout=10)
 
-                await ctx.send('Acertou')
+                if msg.content == 'hangman parar':
+                    break
+
+                await channel.send('Acertou')
+                rank[msg.author.name] += 1
                 words_to_be_guessed.pop(0)
+                hints.pop(0)
+                asyncio.sleep(3)
             except asyncio.TimeoutError:
-                await ctx.send(characters_to_be_guessed)
-                await ctx.send('Acabou o tempo')
+                words_to_be_guessed.pop(0)
+                hints.pop(0)
+                await channel.send('Acabou o tempo!\nA palavra era ' + ''.join(characters_to_be_guessed))
+                asyncio.sleep(3)
+        
+        await ctx.send('Fim de jogo')
+
+        for player, score in rank.items():
+                string_rank = '**{}**: {}'.format(player, score)
+
+        string_final = '**RANKING FINAL**\n\n' + string_rank
+
+        await ctx.send(string_final)
 
 def setup(bot):
     bot.add_cog(Hangman(bot))
