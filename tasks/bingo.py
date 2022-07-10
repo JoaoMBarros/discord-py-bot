4a88a9e1940e4e553b3d9d6ba8847fddd1813639
João Pedro <joao2015.76@gmail.com>
2022-07-09 22:05:47 -0300

    New minigame

---
+++ tasks/bingo.py
@@ -0,0 +1,20 @@
+# New minigame, bingo.
+
+
+"""Initial idea:
+
+- As pessoas vão participar clicando na reação que vai aparecer de bingo
+
+- Faz o bot gerar uma sequencia de números (sei lá, de 01 até 50, tem que ver nos bingos como que é esse intervalo) e envia no privado de quem reagiu em formato de cartela. Pode ser de texto mesmo, tlg, faz o bot criar algo assim:
+01    13    14
+25    30    35
+47    48    50
+
+- Cria uma sala só pra bingo e o bingo rolaria lá, com o bot fazendo o papel de um locutor/mc/apresentador n sei, tipo
+bola 13
+marque na sua cartela bola 13 (poderia ser imagem já feita tbm, amigo, já que não seriam pesadas)
+
+- Quando faltar tipo 2 ou 3 números pra terminar, ele começa a dizer que tem duas pessoas perto de vencer
+
+- Quando o vencedor ou os vencedores completarem o bingo, o bot avisa
+"""
